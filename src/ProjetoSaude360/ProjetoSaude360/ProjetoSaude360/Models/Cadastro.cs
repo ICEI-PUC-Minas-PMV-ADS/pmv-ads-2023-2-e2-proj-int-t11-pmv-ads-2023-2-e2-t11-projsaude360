bcdade4532837eaa4f4874e2457af1a47c3cdbce
Pedro Henrique <pedrohccma@gmail.com>
2023-11-08 18:15:43 -0300

    Criação da pagina Index e sua estilização(html e css), adição da pasta imagens e suas imagens correspondentes

---
+++ src/ProjetoSaude360/ProjetoSaude360/ProjetoSaude360/Models/Cadastro.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoSaude360.Models
+{
+    [Table ("Usuarios")]
+    public class Cadastro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]  
+        public string Senha { get; set; }
+
+        [Required]
+        public Enums.Genero Genero { get; set; }
+
+        [Required]
+        public DateTime DataDeNascimento { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public int Telefone { get; set; }
+
+        public Enums.Perfil Perfil { get; set; }
+    }
+
+    
+}
---
+++ src/ProjetoSaude360/ProjetoSaude360/ProjetoSaude360/Models/Enums.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjetoSaude360.Models
+{
+    public class Enums
+    {
+        public enum Genero { 
+            Masculino,
+            Feminino,
+            Outros
+        }
+
+        public enum Perfil
+        {
+            Admin,
+            User
+        }
+    }
+}

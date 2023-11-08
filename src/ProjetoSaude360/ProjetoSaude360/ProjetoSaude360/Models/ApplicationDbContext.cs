3b30980d04f218d23951ee3fee801b0399c644e4
Pedro Henrique <pedrohccma@gmail.com>
2023-11-08 18:48:32 -0300

    Criação da conexao com banco de dados, seu contexto e sua estrutura

---
+++ src/ProjetoSaude360/ProjetoSaude360/ProjetoSaude360/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoSaude360.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<Cadastro> Cadastros { get; set; }
+    }
+
+
+}

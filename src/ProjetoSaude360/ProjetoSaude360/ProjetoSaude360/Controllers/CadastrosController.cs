df7336cd87c4a1d2de8cd0f7b2c2f57380171761
Pedro Henrique <pedrohccma@gmail.com>
2023-11-09 12:16:07 -0300

    Recriação do CRUD de Cadastro e edição das paginas Create e Edit

---
+++ src/ProjetoSaude360/ProjetoSaude360/ProjetoSaude360/Controllers/CadastrosController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetoSaude360.Models;
+
+namespace ProjetoSaude360.Controllers
+{
+    public class CadastrosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Enums.Genero Generos { get; set; }
+
+        public Enums.Perfil Perfis { get; set; }
+
+        public CadastrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cadastros
+        public async Task<IActionResult> Index()
+        {
+              return _context.Cadastros != null ? 
+                          View(await _context.Cadastros.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Cadastros'  is null.");
+        }
+
+        // GET: Cadastros/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Cadastros == null)
+            {
+                return NotFound();
+            }
+
+            var cadastro = await _context.Cadastros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cadastro == null)
+            {
+                return NotFound();
+            }
+
+            return View(cadastro);
+        }
+
+        // GET: Cadastros/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cadastros/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Senha,Genero,DataDeNascimento,Email,Telefone,Perfil")] Cadastro cadastro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cadastro);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cadastro);
+        }
+
+        // GET: Cadastros/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Cadastros == null)
+            {
+                return NotFound();
+            }
+
+            var cadastro = await _context.Cadastros.FindAsync(id);
+            if (cadastro == null)
+            {
+                return NotFound();
+            }
+            return View(cadastro);
+        }
+
+        // POST: Cadastros/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Senha,Genero,DataDeNascimento,Email,Telefone,Perfil")] Cadastro cadastro)
+        {
+            if (id != cadastro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cadastro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CadastroExists(cadastro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cadastro);
+        }
+
+        // GET: Cadastros/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Cadastros == null)
+            {
+                return NotFound();
+            }
+
+            var cadastro = await _context.Cadastros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cadastro == null)
+            {
+                return NotFound();
+            }
+
+            return View(cadastro);
+        }
+
+        // POST: Cadastros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Cadastros == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Cadastros'  is null.");
+            }
+            var cadastro = await _context.Cadastros.FindAsync(id);
+            if (cadastro != null)
+            {
+                _context.Cadastros.Remove(cadastro);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CadastroExists(int id)
+        {
+          return (_context.Cadastros?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}

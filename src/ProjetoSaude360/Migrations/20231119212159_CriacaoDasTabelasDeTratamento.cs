84c9db113a2dbaec6740275ba3c218495742a37e
Pedro Henrique <pedrohccma@gmail.com>
2023-11-19 18:22:56 -0300

    Migrations Tratamento, Medicamentos e Consultas

---
+++ src/ProjetoSaude360/Migrations/20231119212159_CriacaoDasTabelasDeTratamento.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoSaude360.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoDasTabelasDeTratamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios");
+
+            migrationBuilder.RenameTable(
+                name: "Usuarios",
+                newName: "Cadastros");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cadastros",
+                table: "Cadastros",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Consultas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeMedico = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MotivoConsulta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataConsulta = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Recomendacoes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CadastrosId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consultas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consultas_Cadastros_CadastrosId",
+                        column: x => x.CadastrosId,
+                        principalTable: "Cadastros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tratamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Dosagem = table.Column<double>(type: "float", nullable: false),
+                    DataInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataTermino = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Obs = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Administracao = table.Column<int>(type: "int", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    CadastroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tratamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tratamentos_Cadastros_CadastroId",
+                        column: x => x.CadastroId,
+                        principalTable: "Cadastros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medicamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dosagem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Obs = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Info = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TratamentoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medicamentos_Tratamentos_TratamentoId",
+                        column: x => x.TratamentoId,
+                        principalTable: "Tratamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consultas_CadastrosId",
+                table: "Consultas",
+                column: "CadastrosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medicamentos_TratamentoId",
+                table: "Medicamentos",
+                column: "TratamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tratamentos_CadastroId",
+                table: "Tratamentos",
+                column: "CadastroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consultas");
+
+            migrationBuilder.DropTable(
+                name: "Medicamentos");
+
+            migrationBuilder.DropTable(
+                name: "Tratamentos");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cadastros",
+                table: "Cadastros");
+
+            migrationBuilder.RenameTable(
+                name: "Cadastros",
+                newName: "Usuarios");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios",
+                column: "Id");
+        }
+    }
+}

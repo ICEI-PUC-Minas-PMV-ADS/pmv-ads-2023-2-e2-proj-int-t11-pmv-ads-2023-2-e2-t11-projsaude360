d80beb34ada26d5cdf6a401c51276d978cc28b84
Pedro Henrique <pedrohccma@gmail.com>
2023-11-20 15:58:23 -0300

    Migrations da criacao dos construtores

---
+++ src/ProjetoSaude360/Migrations/20231119221029_CorrecaoTabelasConstrutor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoSaude360.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrecaoTabelasConstrutor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

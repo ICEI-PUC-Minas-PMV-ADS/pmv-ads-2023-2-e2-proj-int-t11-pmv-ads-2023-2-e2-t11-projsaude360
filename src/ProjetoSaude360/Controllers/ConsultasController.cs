b502e03e1485bcf05e9f5b92b691c872fa7498e4
Pedro Henrique <pedrohccma@gmail.com>
2023-11-21 16:12:24 -0300

    Criação dos controller e View dos Medicamentos, Cnsultas e Tratamentos

---
+++ src/ProjetoSaude360/Controllers/ConsultasController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetoSaude360.Models;
+
+namespace ProjetoSaude360.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ConsultasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Consultas
+        public async Task<IActionResult> Index()
+        {
+              return _context.Consultas != null ? 
+                          View(await _context.Consultas.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Consultas'  is null.");
+        }
+
+        // GET: Consultas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Consultas == null)
+            {
+                return NotFound();
+            }
+
+            var consulta = await _context.Consultas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (consulta == null)
+            {
+                return NotFound();
+            }
+
+            return View(consulta);
+        }
+
+        // GET: Consultas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Consultas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NomeMedico,MotivoConsulta,DataConsulta,Recomendacoes")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(consulta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(consulta);
+        }
+
+        // GET: Consultas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Consultas == null)
+            {
+                return NotFound();
+            }
+
+            var consulta = await _context.Consultas.FindAsync(id);
+            if (consulta == null)
+            {
+                return NotFound();
+            }
+            return View(consulta);
+        }
+
+        // POST: Consultas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NomeMedico,MotivoConsulta,DataConsulta,Recomendacoes")] Consulta consulta)
+        {
+            if (id != consulta.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(consulta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConsultaExists(consulta.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(consulta);
+        }
+
+        // GET: Consultas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Consultas == null)
+            {
+                return NotFound();
+            }
+
+            var consulta = await _context.Consultas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (consulta == null)
+            {
+                return NotFound();
+            }
+
+            return View(consulta);
+        }
+
+        // POST: Consultas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Consultas == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Consultas'  is null.");
+            }
+            var consulta = await _context.Consultas.FindAsync(id);
+            if (consulta != null)
+            {
+                _context.Consultas.Remove(consulta);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConsultaExists(int id)
+        {
+          return (_context.Consultas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ src/ProjetoSaude360/Controllers/TratamentosController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetoSaude360.Models;
+
+namespace ProjetoSaude360.Controllers
+{
+    public class TratamentosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TratamentosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tratamentos
+        public async Task<IActionResult> Index()
+        {
+              return _context.Tratamentos != null ? 
+                          View(await _context.Tratamentos.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Tratamentos'  is null.");
+        }
+
+        // GET: Tratamentos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Tratamentos == null)
+            {
+                return NotFound();
+            }
+
+            var tratamento = await _context.Tratamentos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tratamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(tratamento);
+        }
+
+        // GET: Tratamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tratamentos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Dosagem,DataInicio,DataTermino,Obs,Administracao,Tipo")] Tratamento tratamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tratamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tratamento);
+        }
+
+        // GET: Tratamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Tratamentos == null)
+            {
+                return NotFound();
+            }
+
+            var tratamento = await _context.Tratamentos.FindAsync(id);
+            if (tratamento == null)
+            {
+                return NotFound();
+            }
+            return View(tratamento);
+        }
+
+        // POST: Tratamentos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Dosagem,DataInicio,DataTermino,Obs,Administracao,Tipo")] Tratamento tratamento)
+        {
+            if (id != tratamento.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tratamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TratamentoExists(tratamento.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tratamento);
+        }
+
+        // GET: Tratamentos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Tratamentos == null)
+            {
+                return NotFound();
+            }
+
+            var tratamento = await _context.Tratamentos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tratamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(tratamento);
+        }
+
+        // POST: Tratamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Tratamentos == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Tratamentos'  is null.");
+            }
+            var tratamento = await _context.Tratamentos.FindAsync(id);
+            if (tratamento != null)
+            {
+                _context.Tratamentos.Remove(tratamento);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TratamentoExists(int id)
+        {
+          return (_context.Tratamentos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}

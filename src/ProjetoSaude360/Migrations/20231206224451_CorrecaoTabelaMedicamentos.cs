31b1e6d613a39e8d76521fca5f641215a2203657
Pedro Henrique <pedrohccma@gmail.com>
2023-12-06 20:37:46 -0300

    Correção da migration de criacao de medicamentos

---
+++ src/ProjetoSaude360/Migrations/20231206224451_CorrecaoTabelaMedicamentos.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoSaude360.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrecaoTabelaMedicamentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Medicamentos_Medicamentos_MedicamentoId",
+                table: "Medicamentos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Medicamentos_MedicamentoId",
+                table: "Medicamentos");
+
+            migrationBuilder.DropColumn(
+                name: "MedicamentoId",
+                table: "Medicamentos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MedicamentoId",
+                table: "Medicamentos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medicamentos_MedicamentoId",
+                table: "Medicamentos",
+                column: "MedicamentoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Medicamentos_Medicamentos_MedicamentoId",
+                table: "Medicamentos",
+                column: "MedicamentoId",
+                principalTable: "Medicamentos",
+                principalColumn: "Id");
+        }
+    }
+}
